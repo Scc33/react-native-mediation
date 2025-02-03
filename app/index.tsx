49fbd41d05b6a787f052fc51a495399f43e32c76
Sean Coughlin <coughlinscc33@gmail.com>
2025-02-02 20:40:23 -0600

    Update home screen design and navigation

--- app/index.tsx
+++ app/index.tsx
@@ -1,11 +1,13 @@
 import { View, Text, ImageBackground, SafeAreaView } from "react-native";
-import { LinearGradient } from "expo-linear-gradient";
 import { StatusBar } from "expo-status-bar";
 
 import beachImage from "@/assets/meditation-images/beach.webp";
 import CustomButton from "@/components/CustomButton";
+import { useRouter } from "expo-router";
 
 export default function HomeScreen() {
+  const router = useRouter();
+
   return (
     <View className="flex-1">
       <ImageBackground
@@ -13,28 +15,23 @@ export default function HomeScreen() {
         resizeMode="cover"
         className="flex-1"
       >
-        <LinearGradient
-          className="flex-1"
-          colors={["rgba(0,0,0,0.4)", "rgba(0,0,0,0.8)"]}
-        >
-          <SafeAreaView className="flex-1 px-1 justify-between">
-            <View>
-              <Text className="text-center text-white font-bold text-4xl">
-                Meditations
-              </Text>
-              <Text className="text-center text-white text-2xl mt-3">
-                Simplifying Meditation for Everyone
-              </Text>
-            </View>
-            <View>
-              <CustomButton
-                onPress={() => console.log("tap")}
-                title="Get Started"
-              />
-            </View>
-            <StatusBar style="light" />
-          </SafeAreaView>
-        </LinearGradient>
+        <SafeAreaView className="flex-1 mx-5 my-12 justify-between">
+          <View>
+            <Text className="text-center text-white font-bold text-4xl">
+              Meditations
+            </Text>
+            <Text className="text-center text-white text-2xl mt-3">
+              Simplifying Meditation for Everyone
+            </Text>
+          </View>
+          <View>
+            <CustomButton
+              onPress={() => router.push("/nature-meditate")}
+              title="Get Started"
+            />
+          </View>
+          <StatusBar style="light" />
+        </SafeAreaView>
       </ImageBackground>
     </View>
   );
