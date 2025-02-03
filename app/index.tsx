ae08a8b25e69f57946aabe843d3ef9e1b6dfd5ed
Sean Coughlin <coughlinscc33@gmail.com>
2025-02-02 20:29:57 -0600

    Add home screen with background image and gradient overlay

--- app/index.tsx
+++ app/index.tsx
@@ -1,9 +1,41 @@
-import { View, Text } from "react-native";
+import { View, Text, ImageBackground, SafeAreaView } from "react-native";
+import { LinearGradient } from "expo-linear-gradient";
+import { StatusBar } from "expo-status-bar";
+
+import beachImage from "@/assets/meditation-images/beach.webp";
+import CustomButton from "@/components/CustomButton";
 
 export default function HomeScreen() {
   return (
-    <View className="flex-1 items-center justify-center bg-white">
-      <Text>Hello</Text>
+    <View className="flex-1">
+      <ImageBackground
+        source={beachImage}
+        resizeMode="cover"
+        className="flex-1"
+      >
+        <LinearGradient
+          className="flex-1"
+          colors={["rgba(0,0,0,0.4)", "rgba(0,0,0,0.8)"]}
+        >
+          <SafeAreaView className="flex-1 px-1 justify-between">
+            <View>
+              <Text className="text-center text-white font-bold text-4xl">
+                Meditations
+              </Text>
+              <Text className="text-center text-white text-2xl mt-3">
+                Simplifying Meditation for Everyone
+              </Text>
+            </View>
+            <View>
+              <CustomButton
+                onPress={() => console.log("tap")}
+                title="Get Started"
+              />
+            </View>
+            <StatusBar style="light" />
+          </SafeAreaView>
+        </LinearGradient>
+      </ImageBackground>
     </View>
   );
 }
